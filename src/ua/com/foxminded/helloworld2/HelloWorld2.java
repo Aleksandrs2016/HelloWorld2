42a814f6e957425614c9892a19d78413092285e9
Aleksandrs2016 <korsaks.aleksandrs@gmail.com>
2021-11-11 19:02:45 +0200

    Initial commit

---
+++ src/ua/com/foxminded/helloworld2/HelloWorld2.java
@@ -0,0 +1,14 @@
+package ua.com.foxminded.helloworld2;
+
+public class HelloWorld2 {
+
+	public static void main(String[] args) {
+		String message = "Helo World!";
+		String words = "Java forever";
+		String space = " ";
+		System.out.println(message);
+		System.out.println(words);
+		System.out.println(message + space + words);
+	}
+
+}
